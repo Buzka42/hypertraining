5bff992227c9484b3c76d81ac56e18fa2689b8ce
Arawn <arawn@example.com>
2025-07-09 21:07:08 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -5,11 +5,7 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
     unoptimized: true
-  },
-  output: 'export',
-  trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/hypertraining' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/hypertraining/' : '',
+  }
 }
 
 module.exports = nextConfig
